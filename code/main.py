2dae307cdf27f13230a05cdfa981ffbd59aa7814
jaeymax <azagojunior2@gmail.com>
2023-10-22 11:28:43 -0400

    added line to print even numbers in numbers list

--- code/main.py
+++ code/main.py
@@ -1,2 +1,9 @@
 print('Hello Collab')
 print('Hello Jeol')
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#prints the even numbers in the list of numbers
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
